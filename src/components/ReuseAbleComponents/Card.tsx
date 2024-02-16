da83377784c574a23f15660c5d7ccfecc102fffa
okcha4ever <minoukhiari4@gmail.com>
2024-02-16 07:49:14 +0100

    fix: import as type

--- src/components/ReuseAbleComponents/Card.tsx
+++ src/components/ReuseAbleComponents/Card.tsx
@@ -1,5 +1,5 @@
 import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
-import { CompanyProps } from "@/types/Card";
+import { type CompanyProps } from "@/types/Card";
 
 export default function Card({ data }: { data: CompanyProps }) {
   return (
